14606bd6cd08bc5e0ca25e4b32988583b5589036
Asel Gunaratne <aselgunaratne@gmail.com>
2021-11-28 00:14:48 +0530

    create author

--- routes/author.js
+++ routes/author.js
@@ -9,7 +9,32 @@ router.route('/hello').get((req, res) => {
     })
 })
 
+// add new author (POST localhost:5000/book/newbook)
+router.route('/newauthor').post((req, res) => {
+    
+    const { body } = req;
+    const { name } = body; 
+    
+    console.log(req.body);
 
+    if (!name) {
+        return res.send({
+            success: false,
+            message: 'Error: Author Name cannot be blank.'
+        })
+    }
+    const newauthor = new Author(req.body);
+    
+    console.log(newauthor);
+    newauthor.save()
+        .then(() =>
+            res.send({
+                success: true,
+                message: 'New Author added to list.'
+            })
+        )
+        .catch(err => res.status(400).json('Error: ' + err));
+});
 
 
 module.exports = router;
