63ebaecd34eed2f93fb8d8513d2292de968ff743
Asel Gunaratne <aselgunaratne@gmail.com>
2021-11-28 00:14:17 +0530

    creating author model

--- model/author.model.js
+++ model/author.model.js
@@ -2,8 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const authorSchema = new Schema({
-  name: { type: String, required: true }
-}
+  name: { type: String, required: true },
+  genreSpec: { type: String, required: true },
+  authorDesc: { type: String, required: true },
+  nationality: { type: String, required: true },
+  books: {
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'book'
+  },
+},{strict: false}
 );
 
 
