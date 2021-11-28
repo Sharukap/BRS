baf78a2640826b2cd13799bf8fb7c89a54a8d482
leesha.18@itfac.mrt.ac.lk <leesha.18@itfac.mrt.ac.lk>
2021-11-28 11:42:15 +0530

    initial commit

--- model/library.model.js
+++ model/library.model.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const librarySchema = new Schema({
-  name: { type: String, required: true }
+  name: { type: String, required: true },
+  
 }
 );
 
